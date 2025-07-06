73557df8f51cacd8aac5d2fe893e0709b6b15ed9
umadevi-12 <bogathiu@gmail.com>
2025-07-06 11:24:50 +0530

    done

---
+++ unit2/dayy1.html/unit3_Coding/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDCFdaWbi9thMEw5K7wSoQqDK55A7Yhdsc",
+  authDomain: "fir-config-753b8.firebaseapp.com",
+  projectId: "fir-config-753b8",
+  storageBucket: "fir-config-753b8.firebasestorage.app",
+  messagingSenderId: "42155698081",
+  appId: "1:42155698081:web:b1fdc180b9fb03a655cde0",
+  measurementId: "G-56MV3P2TW2"
+};
+firebase .initializeApp(firebaseConfig);
+const db = firebase.database();
